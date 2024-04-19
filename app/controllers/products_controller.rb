230679e32a7ce53e3c7a85b60f7966d998966f30
MistehTimmeh <corybenrae@gmail.com>
2024-04-18 23:03:31 -0500

    Addition of search functionality, with keywords used to find within categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,22 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @products = Product.all
+    search = params[:keyword]
+
+    if params[:category] == "products"
+      @products = @products.includes(:flower_type, :flower_colour).where("flower_types.type_name LIKE ? OR flower_colours.colour_name LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%").references(:flower_type, :flower_colour)
+    end
+
+    if params[:category] == "type_name"
+      @products = @products.includes(:flower_type).where("product.description LIKE ? OR flower_types.type_name LIKE ?", "%#{search}%", "%#{search}%").references(:flower_type)
+    end
+
+    if params[:category] == "colour_name"
+      #@products = @products.where("description LIKE ?", "%#{search}%")
+      @products = @products.includes(:flower_colour).where("product.description LIKE ? OR flower_colours.colour_name LIKE ?", "%#{search}%", "%#{search}%").references(:flower_colour)
+    end
+  end
 end
